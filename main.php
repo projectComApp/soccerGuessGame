1ae3742b9067a9a9775c655af871e6c5d5ac9762
Pandaree <neen_ppm@hotmail.com>
2014-03-11 20:26:16 +0700

    add header and footer

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+	require('header.php');
+?>
+  <div class = "container" style="height:60%;">
+	
+	<div id = "topFive">
+		Rank Top Five
+	</div>
+
+	
+</div>
+<?php
+	require('footer.php');
+?>
