78a0ada22aee3407c7a723ef126af79345e08a20
Pandaree <neen_ppm@hotmail.com>
2014-03-28 19:52:39 +0700

    bow2

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<style rel="stylesheet" type = "text/css">
+		 .header{
+		 height: 100px;
+		 background-color: red;
+		 }
+		</style>
+	</head>
+	<header>
+		
+	</header>
+	<body>
+		
+	</body>
+</html>
